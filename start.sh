937397cd2f19924614ab2dac105a450377eb5633
jan@raspy.ch <jan@raspy.ch>
2020-06-10 20:02:11 +0200

    Update script

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Startscript for the Anki-MQTT interface
+# Strübin Jan, 11.06.20
+
+TIMEOUT=30
 
 if [ $# -eq 0 ]
 then
@@ -8,6 +12,6 @@ fi
 
 echo "starting gateway..."
 nohup sudo ./gradlew server & 
-sleep 30
+sleep $TIMEOUT
 echo "starting host service ..."
 java -jar build/libs/Anki_MQTT_Interface.jar $1
